83942daa9d2d557ef36b8d1490cb6cc9ee424ecd
Asbanesh Joel <asbaneshjoel2006@gmail.com>
2026-06-25 03:09:55 +0530

    Spam email classifier 1st commit

---
+++ spam_classifier.py
@@ -0,0 +1,61 @@
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score
+
+# Load dataset
+
+df = pd.read_csv("spam.csv", encoding="latin-1")
+
+# Keep required columns
+
+df = df[['v1', 'v2']]
+
+# Rename columns
+
+df.columns = ['label', 'message']
+
+# Convert labels
+
+df['label'] = df['label'].map({
+    'ham': 0,
+    'spam': 1
+})
+
+# Features and target
+
+X = df['message']
+y = df['label']
+
+# Convert text into numbers
+
+vectorizer = TfidfVectorizer()
+
+X = vectorizer.fit_transform(X)
+
+# Train-Test Split
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+    test_size=0.2,
+    random_state=42
+)
+
+# Train Model
+
+model = MultinomialNB()
+
+model.fit(X_train, y_train)
+
+# Predictions
+
+y_pred = model.predict(X_test)
+
+# Accuracy
+
+accuracy = accuracy_score(y_test, y_pred)
+
+print("Accuracy:", accuracy)
